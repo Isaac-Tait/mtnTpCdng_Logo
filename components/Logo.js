48a6d9f67eb69a96a8fab5d7bf44653807062556
Isaac Tait <isaac@mountaintopcoding.com>
2023-02-13 13:34:02 -0500

    feat: add custom font 🦋

--- components/Logo.js
+++ components/Logo.js
@@ -1,15 +1,17 @@
 const Logo = () => {
   return (
-    <div className="flex text-4xl font-extrabold p-2">
-      <h1 className="tracking-widest inline-block text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400">
+    <div className="flex text-8xl font-extrabold p-20">
+      <h1 className="font-julee tracking-widest inline-block text-transparent bg-clip-text bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400">
         mountainTopCoding(
       </h1>
-      <p className="align-center">
+      <p className="-mt-3">
         <span role="img" aria-label="mountain with snow-cap">
           &#127956;
         </span>
       </p>
-      <h1 className="text-transparent bg-clip-text bg-indigo-400">);</h1>
+      <h1 className="font-julee text-transparent bg-clip-text bg-indigo-400 ml-2">
+        );
+      </h1>
     </div>
   );
 };
